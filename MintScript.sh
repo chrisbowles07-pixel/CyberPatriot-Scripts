e4f5f54b2f01afed7dee6fd85ad639f8db75af16
CTBrocks <chrisbowles07@gmail.com>
2026-01-16 08:03:51 -0600

    Remove user management features from MintScript.sh
    Removed user management functionality for existing and new users.

--- MintScript.sh
+++ MintScript.sh
@@ -58,88 +58,8 @@ chmod 777 ~/Desktop/backups/group
 cp /etc/passwd ~/Desktop/backups/
 chmod 777 ~/Desktop/backups/passwd
 
-printTime "/etc/group and /etc/passwd files backed up."
+printTime "/etc/group and /etc/passwd files backed up.
 
-echo Type all user account names, with a space in between
-read -a users
-
-usersLength=${#users[@]}	
-
-for (( i=0;i<$usersLength;i++))
-do
-	clear
-	echo ${users[${i}]}
-	echo Delete ${users[${i}]}? yes or no
-	read yn1
-	if [ $yn1 == yes ]
-	then
-		userdel -r ${users[${i}]}
-		printTime "${users[${i}]} has been deleted."
-	else	
-		echo Make ${users[${i}]} administrator? yes or no
-		read yn2								
-		if [ $yn2 == yes ]
-		then
-			gpasswd -a ${users[${i}]} sudo
-			gpasswd -a ${users[${i}]} adm
-			gpasswd -a ${users[${i}]} lpadmin
-			gpasswd -a ${users[${i}]} sambashare
-			printTime "${users[${i}]} has been made an administrator."
-		else
-			gpasswd -d ${users[${i}]} sudo
-			gpasswd -d ${users[${i}]} adm
-			gpasswd -d ${users[${i}]} lpadmin
-			gpasswd -d ${users[${i}]} sambashare
-			gpasswd -d ${users[${i}]} root
-			printTime "${users[${i}]} has been made a standard user."
-		fi
-		
-		echo Make custom password for ${users[${i}]}? yes or no
-		read yn3								
-		if [ $yn3 == yes ]
-		then
-			echo Password:
-			read pw
-			echo -e "$pw\n$pw" | passwd ${users[${i}]}
-			printTime "${users[${i}]} has been given the password '$pw'."
-		else
-			echo -e "Cyb3rPatr!ots\nCyb3rPatr!ots" | passwd ${users[${i}]}
-			printTime "${users[${i}]} has been given the password 'Cyb3rPatr!ots'."
-		fi
-		passwd -x30 -n3 -w7 ${users[${i}]}
-		printTime "${users[${i}]}'s password has been given a maximum age of 30 days, minimum of 3 days, and warning of 7 days. ${users[${i}]}'s account has been locked."
-	fi
-done
-clear
-
-echo Type user account names of users you want to add, with a space in between
-read -a usersNew
-
-usersNewLength=${#usersNew[@]}	
-
-for (( i=0;i<$usersNewLength;i++))
-do
-	clear
-	echo ${usersNew[${i}]}
-	adduser ${usersNew[${i}]}
-	printTime "A user account for ${usersNew[${i}]} has been created."
-	clear
-	echo Make ${usersNew[${i}]} administrator? yes or no
-	read ynNew								
-	if [ $ynNew == yes ]
-	then
-		gpasswd -a ${usersNew[${i}]} sudo
-		gpasswd -a ${usersNew[${i}]} adm
-		gpasswd -a ${usersNew[${i}]} lpadmin
-		gpasswd -a ${usersNew[${i}]} sambashare
-		printTime "${usersNew[${i}]} has been made an administrator."
-	else
-		printTime "${usersNew[${i}]} has been made a standard user."
-	fi
-	
-	passwd -x30 -n3 -w7 ${usersNew[${i}]}
-	printTime "${usersNew[${i}]}'s password has been given a maximum age of 30 days, minimum of 3 days, and warning of 7 days. ${users[${i}]}'s account has been locked."
-done
 
 echo Does this machine need Samba?
 read sambaYN
