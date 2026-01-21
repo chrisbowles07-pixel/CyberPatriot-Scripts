19bb973390c499c011a5db1c8420299fab7f8707
CTBrocks <chrisbowles07@gmail.com>
2026-01-21 08:19:06 -0600

    Refactor user management and enhance firewall setup
    Removed user management functionality and added firewall setup.

--- MintScript.sh
+++ MintScript.sh
@@ -60,57 +60,6 @@ chmod 777 ~/Desktop/backups/passwd
 
 printTime "/etc/group and /etc/passwd files backed up."
 
-echo Type all user account names, with a space in between
-read -a users
-
-usersLength=${#users[@]}	
-
-for (( i=0;i<$usersLength;i++))
-do
-	clear
-	echo ${users[${i}]}
-	echo Delete ${users[${i}]}? yes or no
-	read yn1
-	if [ $yn1 == yes ]
-	then
-		userdel -r ${users[${i}]}
-		printTime "${users[${i}]} has been deleted."
-	else	
-		echo Make ${users[${i}]} administrator? yes or no
-		read yn2								
-		if [ $yn2 == yes ]
-		then
-			gpasswd -a ${users[${i}]} sudo
-			gpasswd -a ${users[${i}]} adm
-			gpasswd -a ${users[${i}]} lpadmin
-			gpasswd -a ${users[${i}]} sambashare
-			printTime "${users[${i}]} has been made an administrator."
-		else
-			gpasswd -d ${users[${i}]} sudo
-			gpasswd -d ${users[${i}]} adm
-			gpasswd -d ${users[${i}]} lpadmin
-			gpasswd -d ${users[${i}]} sambashare
-			gpasswd -d ${users[${i}]} root
-			printTime "${users[${i}]} has been made a standard user."
-		fi
-		
-		echo Make custom password for ${users[${i}]}? yes or no
-		read yn3								
-		if [ $yn3 == yes ]
-		then
-			echo Password:
-			read pw
-			echo -e "$pw\n$pw" | passwd ${users[${i}]}
-			printTime "${users[${i}]} has been given the password '$pw'."
-		else
-			echo -e "Cyb3rPatr!ots\nCyb3rPatr!ots" | passwd ${users[${i}]}
-			printTime "${users[${i}]} has been given the password 'Cyb3rPatr!ots'."
-		fi
-		passwd -x30 -n3 -w7 ${users[${i}]}
-		printTime "${users[${i}]}'s password has been given a maximum age of 30 days, minimum of 3 days, and warning of 7 days. ${users[${i}]}'s account has been locked."
-	fi
-done
-clear
 
 echo Type user account names of users you want to add, with a space in between
 read -a usersNew
@@ -193,6 +142,7 @@ echo 'exit 0' >> /etc/rc.local
 printTime "Any startup scripts have been removed."
 
 clear
+apt update
 apt-get install ufw -y -qq
 ufw enable
 ufw deny 1337
@@ -272,6 +222,7 @@ then
 	printTime "SSH port has been denied on the firewall. Open-SSH has been removed."
 elif [ $sshYN == yes ]
 then
+	apt install openssh-server
 	ufw allow ssh
 	cp /etc/ssh/sshd_config ~/Desktop/backups/	
 	grep PermitRootLogin /etc/ssh/sshd_config | grep yes
@@ -302,6 +253,7 @@ then
 else
 	echo Response not recognized.
 fi
+gnome-terminal -- bash -c 'systemctl status ssh;
 printTime "SSH is complete."
 
 clear
